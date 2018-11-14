61e905cc4ce9d0fedae819a323039506e4bdf23b
Dennis Wolfe <denni01wolfe@gmail.com>
2018-11-14 11:05:17 -0500

    Textures Exercise 1: Flip image over horizontal axis using only fragment shader

--- LearnOpenGL/LearnOpenGL/fragment.fs
+++ LearnOpenGL/LearnOpenGL/fragment.fs
@@ -7,5 +7,5 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 void main()
 {
-    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.4) * vec4(ourColor, 1.0);
+    FragColor = mix(texture(texture1, texCoord), texture(texture2, vec2(1 - texCoord.x, texCoord.y)), 0.4) * vec4(ourColor, 1.0);
 }
