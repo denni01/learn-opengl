110d6d8f932eab0346a1005574939bb323f29e34
Dennis Wolfe <denni01wolfe@gmail.com>
2018-11-14 10:30:16 -0500

    Add a texture to two triangles that form a rectangle. Add stb image library.

--- LearnOpenGL/LearnOpenGL/fragment.fs
+++ LearnOpenGL/LearnOpenGL/fragment.fs
@@ -1,7 +1,10 @@
 #version 330 core
 out vec4 FragColor;
 in vec3 ourPosition;
+in vec2 texCoord;
+in vec3 ourColor;
+uniform sampler2D ourTexture;
 void main()
 {
-    FragColor = vec4(ourPosition, 1.0);
+    FragColor = texture(ourTexture, texCoord) * vec4(ourColor, 1.0);
 }
--- LearnOpenGL/LearnOpenGL/main.cpp
+++ LearnOpenGL/LearnOpenGL/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "shader.h"
+#include <stb\stb_image.h>
 
 using namespace std;
 
@@ -45,15 +46,24 @@ int main() {
 
 	Shader shader("vertex.vs", "fragment.fs");
 
-	// Vertex Data in the format: x, y, z, r, g, b
+	// Vertex Data
 	float vertices[] = {
-		0.5, -0.5, 0.0, 1.0, 0.0, 0.0,
-		-0.5, -0.5, 0.0, 0.0, 1.0, 0.0,
-		0.0, 0.5, 0.0, 0.0, 0.0, 1.0
+	//   x     y    z    r    g    b    s    t
+		 0.5,  0.5, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
+		 0.5, -0.5, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0,
+		-0.5, -0.5, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+		-0.5,  0.5, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0
 	};
 
 	unsigned int indices[] = {
-		0, 1, 2
+		0, 1, 3,
+		1, 2, 3
+	};
+
+	float textureCoordinates[] = {
+		0.0, 0.0,
+		1.0, 0.0,
+		0.5, 1.0
 	};
 
 	// Create a vertex buffer object, vertex array object, and element buffer object
@@ -72,14 +82,44 @@ int main() {
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 
+	// texture attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
 	// unbind first buffer
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);	
+
+	// create texture and set options
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	// load image
+	int width, height, nrChannels;
+	unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		cout << "Failed to load texture" << endl;
+	}
+
+	stbi_image_free(data);
 
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -101,6 +141,10 @@ int main() {
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 
 	glfwTerminate();
 
