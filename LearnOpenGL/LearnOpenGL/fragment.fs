4019f2dfae06a2266ce5a46e5047ea31a3ec1675
Dennis Wolfe <denni01wolfe@gmail.com>
2018-11-14 10:53:41 -0500

    Render a texture unit (multiple textures) to the triangles

--- LearnOpenGL/LearnOpenGL/fragment.fs
+++ LearnOpenGL/LearnOpenGL/fragment.fs
@@ -3,8 +3,9 @@ out vec4 FragColor;
 in vec3 ourPosition;
 in vec2 texCoord;
 in vec3 ourColor;
-uniform sampler2D ourTexture;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 void main()
 {
-    FragColor = texture(ourTexture, texCoord) * vec4(ourColor, 1.0);
+    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.4) * vec4(ourColor, 1.0);
 }
